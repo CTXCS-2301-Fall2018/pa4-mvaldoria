f38904b025b00407f52f078052ce11881f1179fb
Marc Valdoria <marc.valdoria@ctx.edu>
2018-10-16 19:06:27 -0500

    PA4-2

--- PA4-2.s
+++ PA4-2.s
@@ -1,5 +1,7 @@
 	@ Factorial
-	@ Put the required header information here
+	@ Marc Valdoria
+	@ PA4-2
+	@ 10/22/18
 
 	.extern printf
 	.extern scanf
@@ -17,7 +19,13 @@ main:	PUSH	{LR}
 	@ Your code goes here.  Put n! in R2
 	@-------------
 
+	MOV R2, #1 @ Move #1 into R2 as a default value.
 
+_loop:
+    CMP R0, #1       @ Compare R0 to #1.
+    MULGT R2, R0, R2 @ Multply (R0 * x)if R0 is greater than #1 and store into R1.
+    SUBGT R0, R0, #1 @ Substract (R0 - #1) if R0 is greater than #1 and store into R1.
+    BGT _loop        @ Loop if R0 is greater than #1.
 
 
 	@-------------
@@ -27,7 +35,7 @@ main:	PUSH	{LR}
 	LDR	R0, =out
 	BL 	printf
 	POP	{PC}
-	
+
 .data
 n:	.word 0
 prompt:	.asciz "Enter n: "
